df1c5814ae72b33856e3645dc5d1cbae43c1aab9
Celia Cintas <cintas.celia@gmail.com>
2017-11-29 15:18:01 +0100

    plotting func

--- src/network.py
+++ src/network.py
@@ -3,6 +3,9 @@ import theano
 from nolearn.lasagne import BatchIterator
 from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import explained_variance_score, r2_score
+import matplotlib.pyplot as plt
 
 
 class AdjustVariable(object):
@@ -56,3 +59,60 @@ class EarlyStopping(object):
                 self.best_valid, self.best_valid_epoch))
             nn.load_params_from(self.best_weights)
             raise StopIteration()
+
+
+class CNN(object):
+    def __init__(self, *args):
+        try:
+            self.args = dict(args)
+        except TypeError:
+            print("load args as tuples")
+
+    def create_custom_net(self, layers):
+        pass
+
+    def create_best_net(self):
+        # TODO crossval till we have the RL
+        pass
+
+    def training(self):
+        pass
+
+    def plot_loss(self, x_label='epoch', y_label='loss',
+                  out='/tmp/loss_plot.png'):
+        plt.clf()
+        train_loss = np.array([i["train_loss"] for i in self.cnn_net.train_history_])
+        valid_loss = np.array([i["valid_loss"] for i in self.cnn_net.train_history_])
+        plt.plot(train_loss, linewidth=3, label="train")
+        plt.plot(valid_loss, linewidth=3, label="valid")
+        plt.grid()
+        plt.legend()
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.yscale("log")
+        plt.savefig(out)
+
+    def plot_samples(self, y_pred, ids=None, out_folder='/tmp/'):
+        # TODO fix this to put names when predict stage
+        for i in range(y_pred.shape[0]):
+            plt.clf()
+            fig = plt.figure(figsize=(6, 3))
+            ax = fig.add_subplot(1, 2, 1, xticks=[], yticks=[])
+            img = x[i].reshape(self.image_size)
+            ax.imshow(img, cmap='gray')
+            # TODO apply minmax scaler before plot
+            ax.scatter(y_pred[i][0::2], y_pred[i][1::2], marker='x', s=5)
+            plt.savefig(out_folder + 'lateral_{}.png'.format(str(i)))
+
+    def report_metrics(self, X_test, y_test):
+        # TODO plots should be at other place
+        self.plot_loss(self.cnn_net)
+        y_pred = self.cnn_net.predict(X_test)
+        self.plot_sample(X_test, y_pred, names=None)
+        # TODO this should be over the minmax scale values
+        rmse = np.sqrt(mean_squared_error(y_test.ravel(), y_pred.ravel()))
+        ev = explained_variance_score(y_test.ravel(), y_pred.ravel())
+        mae = mean_absolute_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+
+        return rmse, ev, mae, r2
--- test_dataload.py
+++ test_dataload.py
@@ -1,7 +1,19 @@
 from src.dataload import DataLoader
 
 
-def test_pipeline():
+def test_dataload():
     my_data_loader = DataLoader('data/test_image_landmarks.csv')
     X, y = my_data_loader.load_transform()
     assert (X.shape, y.shape) == ((2735, 9216), (2735, 90))
+
+
+def test_network_build():
+    pass
+
+
+def test_training():
+    pass
+
+
+def test_pipeline():
+    pass
